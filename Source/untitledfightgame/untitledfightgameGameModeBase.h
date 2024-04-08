1ae4b9d0f03ecb8f5bf8a9c736163c6c5ec0ef0c
Archie Jaskowicz <archiejaskowicz@gmail.com>
2024-04-08 14:26:03 +0100

    feat: added project files

---
+++ Source/untitledfightgame/untitledfightgameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "untitledfightgameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNTITLEDFIGHTGAME_API AuntitledfightgameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
