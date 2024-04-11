b1a09a96c5112fef2474e8e92a5e7996d45d86e6
Jaskowicz1 <archiejaskowicz@gmail.com>
2024-04-11 21:48:33 +0100

    feat: Added intro video, added EventsManager

---
+++ Source/untitledfightgame/Private/EventsManager.cpp
@@ -0,0 +1,18 @@
+// Copyright - Tinkering Studios
+
+
+#include "EventsManager.h"
+
+// Sets default values for this component's properties
+UEventsManager::UEventsManager()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+// Called when the game starts
+void UEventsManager::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
---
+++ Source/untitledfightgame/Public/EventsManager.h
@@ -0,0 +1,31 @@
+// Copyright - Tinkering Studios
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EventsManager.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FEventRequested);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEventStarted, FGameplayTag, EventTag);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEventEnded, FGameplayTag, EventTag);
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class UNTITLEDFIGHTGAME_API UEventsManager : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UEventsManager();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Events")
+	TArray<FGameplayTag> EventTags;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+		
+};
