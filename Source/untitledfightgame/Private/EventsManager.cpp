0eb81989cdd11fdc32ba185553b5fb62a23c3bd4
Jaskowicz1 <archiejaskowicz@gmail.com>
2024-04-11 22:10:06 +0100

    feat: Added EventsManager

--- Source/untitledfightgame/Private/EventsManager.cpp
+++ Source/untitledfightgame/Private/EventsManager.cpp
@@ -10,6 +10,45 @@ UEventsManager::UEventsManager()
 }
 
 
+FGameplayTag UEventsManager::RequestEvent()
+{
+	FGameplayTag tag{};
+	
+	if(EventTags.IsEmpty())
+	{
+		// Empty tag at this point
+		return tag;
+	}
+
+	tag = EventTags[FMath::RandRange(0, EventTags.Num()-1)];
+
+	// Somehow, the tag wasn't valid, so exit early so we don't broadcast an invalid tag.
+	if(!tag.IsValid())
+	{
+		// Invalid tag at this point, make sure to always check it's valid!
+		return tag;
+	}
+	
+	OnEventRequested.Broadcast(tag);
+	return tag;
+}
+
+void UEventsManager::StartEvent(const FGameplayTag tag)
+{
+	CurrentEvent = tag;
+	OnEventStarted.Broadcast(CurrentEvent);
+}
+
+void UEventsManager::StopCurrentEvent()
+{
+	OnEventEnded.Broadcast(GetCurrentEvent());
+}
+
+FGameplayTag& UEventsManager::GetCurrentEvent()
+{
+	return CurrentEvent;
+}
+
 // Called when the game starts
 void UEventsManager::BeginPlay()
 {
--- Source/untitledfightgame/Public/EventsManager.h
+++ Source/untitledfightgame/Public/EventsManager.h
@@ -7,7 +7,7 @@
 #include "Components/ActorComponent.h"
 #include "EventsManager.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FEventRequested);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEventRequested, FGameplayTag, EventTag);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEventStarted, FGameplayTag, EventTag);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEventEnded, FGameplayTag, EventTag);
 
@@ -21,12 +21,39 @@ public:
 	// Sets default values for this component's properties
 	UEventsManager();
 
+#pragma region Delegates
+
+	UPROPERTY(BlueprintAssignable)
+	FEventRequested OnEventRequested;
+
+	UPROPERTY(BlueprintAssignable)
+	FEventStarted OnEventStarted;
+
+	UPROPERTY(BlueprintAssignable)
+	FEventEnded OnEventEnded;
+
+#pragma endregion
+
+	UFUNCTION(BlueprintPure, Category="Events")
+	FGameplayTag RequestEvent();
+
+	UFUNCTION(BlueprintCallable, Category="Events")
+	void StartEvent(const FGameplayTag tag);
+
+	UFUNCTION(BlueprintCallable, Category="Events")
+	void StopCurrentEvent();
+
+	UFUNCTION(BlueprintPure, Category="Events")
+	FGameplayTag& GetCurrentEvent();
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Events")
 	TArray<FGameplayTag> EventTags;
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
+
+	FGameplayTag CurrentEvent;
 
 		
 };
