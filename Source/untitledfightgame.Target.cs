fcec6526903d1c422ac7a45837c45f36a9ccb495
Archie Jaskowicz <archiejaskowicz@gmail.com>
2024-05-10 20:21:29 +0100

    feat: updated to 5.4

--- Source/untitledfightgame.Target.cs
+++ Source/untitledfightgame.Target.cs
@@ -8,8 +8,8 @@ public class untitledfightgameTarget : TargetRules
 	public untitledfightgameTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("untitledfightgame");
 	}
 }
